c32d54ebf39431ef95c07dfedec27af368308225
robim16 <carteagajimenez@gmail.com>
2026-01-24 15:31:13 -0500

    creacion del comp. about

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./pages/header/header";
+import { Footer } from "./pages/footer/footer";
+import { Home } from "./components/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
